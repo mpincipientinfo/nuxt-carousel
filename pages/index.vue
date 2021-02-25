cb5dc5e315dff322c1cfa2bfcb7e39ea0fc321b2
bhoomit ganatra <ganatrabhoomit070@gmail.com>
2021-02-25 15:02:16 +0530

    done

---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="carousel-wrapper">
+    <div style="height:100px;width:500px">
+      <client-only>
+        <splide :options="options">
+          <splide-slide>
+            <img src="https://source.unsplash.com/random" /> </splide-slide
+          ><splide-slide>
+            <img src="https://source.unsplash.com/random" /> </splide-slide
+          ><splide-slide>
+            <img src="https://source.unsplash.com/random" /> </splide-slide
+          ><splide-slide>
+            <img src="https://source.unsplash.com/random" />
+          </splide-slide>
+        </splide>
+      </client-only>
+    </div>
+  </div>
+</template>
+
+<script type="module">
+export default {
+  data() {
+    return {
+      options: {
+        infinite: true,
+        type: "loop",
+        perPage: 3,
+        dots: false,
+        perMove: 1,
+        pagination: false
+      }
+    };
+  }
+};
+</script>
+<style>
+.splide__arrow--next {
+  right: -8em !important;
+}
+.splide__arrow--prev {
+  left: -8em !important;
+}
+.splide__arrow {
+  box-sizing: border-box !important;
+  font-weight: 30px;
+  height: 50px;
+  width: 50px;
+  border-radius: 5px;
+  background-color: #555;
+}
+.carousel-wrapper {
+  margin-left: 155px;
+}
+</style>
---
+++ plugins/carrousel.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import VueSlickCarousel from 'vue-slick-carousel'
+import { Splide, SplideSlide } from "@splidejs/vue-splide";
+
+Vue.component('VueSlickCarousel', VueSlickCarousel)
+Vue.component('splide',Splide)
+Vue.component('splide-slide',SplideSlide)
