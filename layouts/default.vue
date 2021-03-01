18f2d8d4d2f27e91296b6891ce27aba3acf819dd
Bhoomil Shah <eincipientinfo@gmail.com>
2021-03-01 13:15:43 +0530

    test

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,11 @@
 <template>
-  <div>
-    <Nuxt />
-  </div>
+  <div><Nuxt />ss</div>
 </template>
 
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
